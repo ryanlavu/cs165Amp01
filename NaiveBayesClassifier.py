98ffaad8cdca494e255682df31b00f2cf3c755b1
ryanlavu <ryanvu@ucsb.edu>
2022-11-08 23:46:11 -0800

    RV

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -6,11 +6,10 @@ from math import exp
 import numpy as np 
 import pandas as pd
 
-
 n = len(sys.argv)
 
-for i in range(1, n):
-    print(sys.argv[i], end = " ")
+#for i in range(1, n):
+#    print(sys.argv[i], end = " ")
 
 file0 = sys.argv[1]
 file1 = sys.argv[2]
@@ -22,6 +21,11 @@ file1Read = pd.read_csv(file1)
 
 #WORK ON LATER, FIRST ROW IS PAVED OVER FOR THE COLUMN LABELS
 file0Read.columns = ['Hotel-Type', 'Arrival-Date-Month', 'Meal-Request', 'Market-Segment-Designation',
+'Booking-Distribution-Channel', 'Reserved-Room-Type', 'Deposit-Type', 'Customer-Type', 'Days-fromReservation-to-Arrival', 
+'Arrival-Date-Week-Number', 'Arrival-Date-Day-of-Month', 'Stays-inWeekend-Nights', 'Stays-in-Weekday-Nights', 'Adults', 
+'Children', 'Babies', 'Is-Repeated-Guest', 'Previous-Cancellations', 'Previous-Booking-Not-Cancelled', 
+'Requested-Car-Parking-Spaces', 'Total-ofSpecial-Requests', 'Average-Daily-Rate', 'Label']
+file1Read.columns = ['Hotel-Type', 'Arrival-Date-Month', 'Meal-Request', 'Market-Segment-Designation',
 'Booking-Distribution-Channel', 'Reserved-Room-Type', 'Deposit-Type', 'Customer-Type', 'Days-fromReservation-to-Arrival', 
 'Arrival-Date-Week-Number', 'Arrival-Date-Day-of-Month', 'Stays-inWeekend-Nights', 'Stays-in-Weekday-Nights', 'Adults', 
 'Children', 'Babies', 'Is-Repeated-Guest', 'Previous-Cancellations', 'Previous-Booking-Not-Cancelled', 
@@ -52,21 +56,22 @@ trainingLikelihoods = {}
 
 prob_class = {}
 #This is the class column
-trainingClass = file0Read[file0Read.columns[-1]]
+trainingClass_Y = file0Read[file0Read.columns[-1]]
 #print("\n", trainingClass)
 
 prob_features = {}
+prob_features_liklihoods = {}
 #This is the features columns
-trainingFeatures = file0Read.iloc[0:file0Read.shape[0],0:22]
+trainingFeatures_X = file0Read.iloc[0:file0Read.shape[0],0:22]
 #print("\n", trainingFeatures)
 
-numTrainingRows = trainingFeatures.shape[0]
-numFeatures = trainingFeatures.shape[1]
+numTrainingRows = trainingFeatures_X.shape[0]
+numFeatures = trainingFeatures_X.shape[1]
 
 #Calculate P(Label = 1) and P(Label = 0)
-for result in np.unique(trainingClass):
-		sum_count = sum(trainingClass == result)
-		prob_class[result] = sum_count / numTrainingRows
+for result in np.unique(trainingClass_Y):
+	sum_count = sum(trainingClass_Y == result)
+	prob_class[result] = sum_count / numTrainingRows
 #print(prob_class)
 
 #Calculate liklihood of all discrete feature values
@@ -76,21 +81,123 @@ for feature in listDiscrete:
     for feature_value in file0Read[feature].unique():
         sum_count = sum(file0Read[feature] == feature_value)
         prob_features[feature][feature_value] = sum_count / numTrainingRows
-print(prob_features)
+#print(prob_features)
 
-#Calculate 
-#for feature in listDiscrete:
-#    prob_features[feature] = {}
-#    for outcome in np.unique(trainingClass):
-#        for feature_value in file0Read[feature].unique():
-#            sum_count = sum(trainingClass == outcome and i for i in )
-#            prob_features[feature][outcome] = sum_count / numTrainingRows
+#Calculate yes no probabilities for each feature
+#print(file0Read['feature'])
+sum_count = 0
+for feature in listDiscrete:
+    prob_features_liklihoods[feature] = {}
+    #for outcome in np.unique(trainingClass_Y):
+    for feature_value in file0Read[feature].unique():
+        #print("OUTCOME",outcome)
+        #print("OUTCOME ", outcome, "\n")
+        #for feature_value in file0Read[feature].unique():
+        for outcome in np.unique(trainingClass_Y):
+            print("FEATURE_VALUE",feature_value)
+            prob_features_liklihoods[feature][feature_value] = {}
+            num_count = sum(file0Read[feature] == feature_value)
+            #FIX LATER
+            sum_count = 0
+            sum_count2 = 0
+            for i in range(numTrainingRows):
+                if(trainingClass_Y[i] == outcome and trainingFeatures_X[feature][i] == feature_value):
+                    sum_count = sum_count + 1
+            sum_count2 = sum(trainingFeatures_X[feature] == feature_value)
+            #sum_count = sum(trainingClass_Y == outcome)
+            prob_features_liklihoods[feature][feature_value][outcome] = sum_count / sum_count2
+            #prob_features_liklihoods[feature][feature_value].append({outcome : sum_count / sum_count2})
+            #prob_features_liklihoods[feature][feature_value].append({1-outcome : 1-sum_count / sum_count2})
+            print("OUTCOME = ", outcome, " AND prob_features_liklihoods = ", prob_features_liklihoods)
+print(prob_features_liklihoods)
 
+#{'Hotel-Type': {'City Hotel_1.0': [{1.0: 0.5494719807464885}, {0.0: 0.64}], 'Resort Hotel': {1.0: 0.39431687206312616}}
+
+#Calculate the mean and std dev of the continuous features
+meanD = {}
+std_dev = {}
+for feature in listCont:
+    meanD[feature] = {}
+    std_dev[feature] = {}
+    for outcome in np.unique(trainingClass_Y):
+        abc = file0Read[file0Read["Label"] == outcome]
+        #FIX MEAN AND STD DEV
+        meanD[feature][outcome] = abc.mean()
+        std_dev[feature][outcome] = abc.std()
+print("MEAN OF ADULTS = 0", meanD["Adults"][0])
+print("STD OF ADULTS = 0", std_dev["Adults"][0])
 
 file0AndBayes_stop = perf_counter()
 print("Elapsed time of opening the training file and training a Naive Bayes classifier: ", file0AndBayes_stop - file0AndBayes_start)
 
+#CALCULATING ACCURACY WITH training.txt
+applyBayesOnfiles_start = perf_counter()
+#print(file0Read)
+#print("Accuracy of built classifier on training.txt\n")
+totalAccurate = 0
+totalAccurateRate = 0
+rowAccuracy = 1
+for ind in file0Read.index:
+    #print(file0Read['Hotel-Type'][ind])
+    rowAccuracy = 1
+    for feature in listFeatures:
+        #rowAccuracy = rowAccuracy * calculate_probability(feature, ind)
+        #rowAccuracy = rowAccuracy * calculate_probability(feature, file0Read[ind])
+        break
+    if(rowAccuracy > 0.5):
+        totalAccurate = totalAccurate + 1
+totalAccurateRate = totalAccurate / numTrainingRows
+print("The accuracy rate of our bayes network on the training set was ", totalAccurateRate*100, "%\n")
 
-applyBayesOnfile1_start = perf_counter()
 
-applyBayesOnfile1_stop = perf_counter()
+numTrainingRows2 = file1Read.shape[0]
+#CALCULATING ACCURACY WITH testing.txt
+print("Accuracy of built classifier on testing.txt\n")
+totalAccurate = 0
+totalAccurateRate = 0
+rowAccuracyYes = 1
+rowAccuracyNo = 1
+
+for ind in range(numTrainingRows2):
+    #print(file0Read['Hotel-Type'][ind])
+    print("IND = ", ind)
+    rowAccuracyYes = 1
+    rowAccuracyNo = 1
+    for feature in listFeatures:
+        #print("FILE1READ.COLUMNS", file1Read.columns)
+        #col = file1Read.columns.get_loc(feature)
+        #print("FILE1READ", file1Read)
+        feat_val = file1Read.loc[ind, feature]
+        #print("FEAT_VAL", feat_val)
+        #print("COLUMNS", col)
+        if feature in listDiscrete:
+            #Discrete
+            #P(label | features) = P(feature1=city hotel | label = yes, no) * P(feature2 | label) ... * P(label)
+            #P(label) = prob_class
+            #P(features | label) = prob_features_liklihoods
+            #print("FILE1READ[ind]=",file1Read[ind], "\nIND", ind)
+            #feature_val_2 = file1Read[ind][col]
+            #feature_val_2 = file1Read.iloc[ind, col]
+            #print("FEATURES_VAL_2", feature_val_2)
+            #print("PROB_FEATURES_LIKLIHOODS", prob_features_liklihoods)
+            #print("FEATURES=", feature, "COL=", col)
+            #print("prob = ",prob_features_liklihoods[feature])
+            #print("prob = ",prob_features_liklihoods[feature][feat_val])
+            rowAccuracyYes = rowAccuracyYes * prob_features_liklihoods[feature][feat_val][1.0]
+            rowAccuracyNo = rowAccuracyNo * (1 - prob_features_liklihoods[feature][feat_val][1.0])
+            print("YES = ", rowAccuracyYes, "NO = ", rowAccuracyNo)
+        if feature in listCont:
+            #Continuous
+            print("con")
+    if (rowAccuracyNo > rowAccuracyYes):
+        if file1Read.iloc[ind]["Label"] == 0:
+            totalAccurate = totalAccurate + 1
+    else:
+        if file1Read.iloc[ind]["Label"] == 1:
+            totalAccurate = totalAccurate + 1
+totalAccurateRate = totalAccurate / numTrainingRows2
+
+
+print("The accuracy rate of our bayes network on the testing set was ", totalAccurateRate*100, "%\n")
+applyBayesOnfiles_stop = perf_counter()
+print("Elapsed time of finding the accuracy of calculated bayes network on both training.txt and testing.txt: ", applyBayesOnfiles_stop - applyBayesOnfiles_start)
